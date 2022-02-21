bd8fa7b8eac8b55b159d6fef52a313fb6bfcd84c
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-20 17:15:17 -0800

    finalize the VC code, now testing

--- JournalApp-2/Controllers/EntryController.swift
+++ JournalApp-2/Controllers/EntryController.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class EntryController {
+    
+    // CRUD : Create, Read, Update, Delete
+    
+    //create
+    static func createEntry(title: String, body: String, journal: Journal) {
+        // create new instance of an entry with what was passed in
+        let timestamp = Date()
+        let newEntry = Entry(title: title, body: body, timestamp: timestamp)
+        // add the new entry to entries array
+        journal.entries.append(newEntry)    // appends to journal that was passed in
+        JournalController.shared.saveToPersistenceStore()
+    }
+    
+    //delete
+    static func deleteEntry(entry: Entry, journal: Journal) {
+        // object must be equatable to use .firstIndex(of: ) to find match in array
+        guard let index = journal.entries.firstIndex(of: entry) else { return }
+        // remove the song from array
+        journal.entries.remove(at: index)
+        JournalController.shared.saveToPersistenceStore()
+    }
+    
+}
