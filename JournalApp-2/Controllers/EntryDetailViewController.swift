7dddf8b2e9780e1b06098f1c8887a46c7870b9c4
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-20 17:21:53 -0800

    add gif of app

--- JournalApp-2/Controllers/EntryDetailViewController.swift
+++ JournalApp-2/Controllers/EntryDetailViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var entryTitleLabel: UILabel!
+    @IBOutlet weak var entryBodyLabel: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
+    private func updateViews() {
+        guard let entry = entry, isViewLoaded else { return }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        entryBodyLabel.backgroundColor = .clear
+        entryTitleLabel.text = entry.title
+        entryBodyLabel.text = formatter.string(from: entry.timestamp) + "\n\n" + entry.body
+        
     }
-    */
+
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
 
 }
+
