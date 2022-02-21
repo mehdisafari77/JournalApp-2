bf168a624a8c12eaf8db4d1dff56c074fc7c0b1c
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-20 17:10:53 -0800

    Add code to entry model

--- JournalApp-2/Models/Entry.swift
+++ JournalApp-2/Models/Entry.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class Entry: Codable {
+    let title: String
+    let body: String
+    let timestamp: Date
+    
+    init(title: String, body: String, timestamp: Date){
+        self.title = title
+        self.body = body
+        self.timestamp = timestamp
+    }
+}
+
+//make entry equatable
+extension Entry: Equatable {
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.title == rhs.title && lhs.body == rhs.body
+    }
+}
