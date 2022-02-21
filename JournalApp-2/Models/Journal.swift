fe932784b7e1800e736a053148dca770d18dd84c
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-20 17:10:09 -0800

    Add code to journal model

--- JournalApp-2/Models/Journal.swift
+++ JournalApp-2/Models/Journal.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+// Needs to be codable to be able to save
+class Journal: Codable {
+    let name: String
+    var entries: [Entry]
+    
+    // initialize as empty array
+    init(name: String, entries: [Entry] = []) {
+        self.name = name
+        self.entries = entries
+    }
+}
+
+//make entry equatable
+extension Journal: Equatable {
+    static func == (lhs: Journal, rhs: Journal) -> Bool {
+        return lhs.name == rhs.name && lhs.entries == rhs.entries
+    }
+}
+
